5f35a689b1b6b9f385fdff23280e3e82d3a41435
Chris <legendarycte@gmail.com>
2025-08-30 22:10:41 +0800

    made the debug statements clearer and ordered them better to inform users when there is a missing invitee

--- bot.py
+++ bot.py
@@ -32,21 +32,22 @@ async def main():
         try: 
             user = await client.get_entity(username)
             result = await client(InviteToChannelRequest(group,[user]))
-            print(f"Added {username} to {target_group}")
+            print(f"Adding {username} to group {target_group}...")
             
             # i have to click through 5 pages of redirect to find there is a property called missing_invitees WTF IS THIS SHIT
             # For future reference
             # https://tl.telethon.dev/constructors/messages/invited_users.html 
             if (result.missing_invitees):
-                print(f"nevermind, failed to add. Trying to send invite link to {username}")
+                print(f"{username} has turned off 'add to group'. Trying to send invite link instead..")
                 try:
                     await client.send_message(username, f"Hi! Please join the group {target_group}")
-                    print(f"sent invite to {username}")
+                    print(f"Succesfully sent invite to {username}")
 
-                    await asyncio.sleep(3)
+                    # await asyncio.sleep(3)
                 except Exception as e:
                     print(f"erm, failed to send invite to {username}: {e}")
-
+            else:
+                print(f"Added {username} successfully")
             await asyncio.sleep(3)
         # This doesnt work. The function literally doesnt even throw its own errors that it has listed in documentation
         # except (UserNotMutualContactError, UserPrivacyRestrictedError) as e:
@@ -55,6 +56,7 @@ async def main():
             
         except Exception as e:
             print(f"hey wtf happened man: {e}")
+
     
 with client:
     client.loop.run_until_complete(main())
